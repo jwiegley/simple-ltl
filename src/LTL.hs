9fcf49aaaf27b648a29beaaad9a60ffe0cd586fa
John Wiegley <johnw@newartisans.com>
2019-04-18 23:10:49 -0700

    Simplify some definitions

--- src/LTL.hs
+++ src/LTL.hs
@@ -3,11 +3,9 @@
 {-# LANGUAGE LambdaCase #-}
 
 module LTL
-  ( LTL(..)
-  , Machine(..)
+  ( Machine(..)
   , Reason(..)
   , Result(..)
-  , compile
   , step
   , run
   , neg
@@ -25,9 +23,12 @@ module LTL
   , always
   , truth
   , eq
+
+  , test
   ) where
 
 import GHC.Generics
+import Prelude hiding (and, or, until)
 
 -- | This formulation of LTL is in positive normal form by construction, and
 --   trivially dualizable. This choice was driven by the following Coq
@@ -35,42 +36,42 @@ import GHC.Generics
 --   from this structure into Coq's logic, over all finite lists:
 --
 --   https://github.com/jwiegley/constructive-ltl/blob/master/src/LTL.v#L69
-data LTL a
-  = Top
-  | Bottom  String
-  | Accept  (a -> LTL a)
-  | Reject  (a -> LTL a)
-  | And     (LTL a) (LTL a)
-  | Or      (LTL a) (LTL a)
-  | Next    (LTL a)
-  | Until   (LTL a) (LTL a)
-  | Release (LTL a) (LTL a)
-  deriving Generic
-
--- jww (2019-04-17): Write this correctly
-instance Show (LTL a) where
-  show = \case
-    Top         -> "Top"
-    Bottom e    -> "Bottom " ++ show e
-    Accept _    -> "Accept"
-    Reject _    -> "Reject"
-    And p q     -> "And " ++ show p ++ " " ++ show q
-    Or  p q     -> "Or " ++ show p ++ " " ++ show q
-    Next p      -> "Next " ++ show p
-    Until   p q -> "Until " ++ show p ++ " " ++ show q
-    Release p q -> "Release " ++ show p ++ " " ++ show q
 
 data Machine a b
   = Stop b
   | Delay (Machine a b)
-  | Query (a -> Machine a b)
+  | Ask (a -> Machine a b)
   deriving (Generic, Functor)
 
-instance Show b => Show (Machine a b) where
-  show = \case
-    Stop b  -> "Stop (" ++ show b ++ ")"
-    Delay m -> "Delay (" ++ show m ++ ")"
-    Query _ -> "Query"
+instance Applicative (Machine a) where
+  pure = Stop
+
+  Stop f  <*> Stop x  = Stop (f x)
+  Stop f  <*> Delay x = Delay (Stop f <*> x)
+  Stop f  <*> Ask x   = Ask $ \a -> Stop f <*> x a
+  Delay f <*> Stop x  = Delay (f <*> Stop x)
+  Delay f <*> Delay x = Delay (f <*> x)
+  Delay f <*> Ask x   = Ask $ \a -> Delay f <*> x a
+  Ask f   <*> Stop x  = Ask $ \a -> f a <*> Stop x
+  Ask f   <*> Delay x = Ask $ \a -> f a <*> Delay x
+  Ask f   <*> Ask x   = Ask $ \a -> f a <*> x a
+
+instance Monad (Machine a) where
+  return = pure
+  Stop m  >>= f = f m
+  Delay m >>= f = Delay (m >>= f)
+  Ask m   >>= f = Ask $ \a -> m a >>= f
+
+step :: Machine a b -> a -> Machine a b
+step m x = case m of
+  Stop r  -> Stop r
+  Delay n -> n
+  Ask f   -> step (f x) x
+
+run :: Machine a b -> [a] -> Machine a b
+run m = \case
+  []     -> m
+  x : xs -> run (step m x) xs
 
 data Reason a
   = HitBottom   String
@@ -85,113 +86,90 @@ data Result a
   | Success
   deriving (Generic, Show)
 
-combine :: Machine a (Result a) -> Machine a (Result a)
-        -> Machine a (Result a)
+type LTL a = Machine a (Result a)
+
+combine :: LTL a -> LTL a -> LTL a
 combine (Stop (Failure e)) _  = Stop (Failure (LeftFailed e))
 combine _ (Stop (Failure e))  = Stop (Failure (RightFailed e))
 combine (Delay f') (Delay g') = Delay (combine f' g')
-combine (Query f') (Query g') = Query (\a -> combine (f' a) (g' a))
-combine f (Query g')          = Query (\a -> combine f (g' a))
-combine (Query f') g          = Query (\a -> combine (f' a) g)
+combine (Ask f') (Ask g')     = Ask (\a -> combine (f' a) (g' a))
+combine f (Ask g')            = Ask (\a -> combine f (g' a))
+combine (Ask f') g            = Ask (\a -> combine (f' a) g)
 combine f' (Stop Success)     = f'
 combine (Stop Success) g'     = g'
 
-select :: Machine a (Result a) -> Machine a (Result a)
-       -> Machine a (Result a)
+select :: LTL a -> LTL a -> LTL a
 select (Stop (Failure e1))
        (Stop (Failure e2))   = Stop (Failure (BothFailed e1 e2))
 select (Stop Success) _      = Stop Success
 select _ (Stop Success)      = Stop Success
 select (Delay f') (Delay g') = Delay (select f' g')
-select (Query f') (Query g') = Query (\a -> select (f' a) (g' a))
-select f (Query g')          = Query (\a -> select f (g' a))
-select (Query f') g          = Query (\a -> select (f' a) g)
+select (Ask f') (Ask g')     = Ask (\a -> select (f' a) (g' a))
+select f (Ask g')            = Ask (\a -> select f (g' a))
+select (Ask f') g            = Ask (\a -> select (f' a) g)
 select (Stop (Failure _)) g' = g'
 select f' (Stop (Failure _)) = f'
 
-compile :: LTL a -> Machine a (Result a)
-compile = \case
-  Top         -> Stop Success
-  Bottom e    -> Stop (Failure (HitBottom e))
-
-  Accept v    -> Query (compile . v)
-  Reject v    -> Query $ \x ->
-    fmap (\case Failure _ -> Success
-                Success   -> Failure (Rejected x))
-         (compile (v x))
-
-  And p q     -> combine (compile p) (compile q)
-  Or p q      -> select (compile p) (compile q)
-
-  Next p      -> Delay (compile p)
-
-  Until p q   -> compile (Or q (And p (Next (Until p q))))
-  Release p q -> compile (And q (Or p (Next (Release p q))))
-
 neg :: LTL a -> LTL a
-neg = \case
-  Top         -> Bottom "neg"
-  Bottom _    -> Top
-  Accept v    -> Reject v
-  Reject v    -> Accept v
-  And p q     -> Or (neg p) (neg q)
-  Or p q      -> And (neg p) (neg q)
-  Next p      -> Next (neg p)
-  Until p q   -> Release (neg p) (neg q)
-  Release p q -> Until (neg p) (neg q)
-
-step :: Machine a b -> a -> Machine a b
-step m x = case m of
-  Stop r  -> Stop r
-  Delay n -> n
-  Query f -> step (f x) x
-
-run :: Machine a b -> [a] -> Machine a b
-run m = \case
-  []     -> m
-  x : xs -> run (step m x) xs
+neg = fmap $ \case
+  Success   -> Failure (HitBottom "neg")
+  Failure _ -> Success
 
 top :: LTL a
-top = Top
+top = Stop Success
+{-# INLINE top #-}
 
-bottom :: LTL a
-bottom = Bottom "bottom"
+bottom :: String -> LTL a
+bottom e = Stop (Failure (HitBottom e))
+{-# INLINE bottom #-}
 
 accept :: (a -> LTL a) -> LTL a
-accept = Accept
+accept = Ask
+{-# INLINE accept #-}
 
 reject :: (a -> LTL a) -> LTL a
-reject = Reject
+reject = neg . Ask
+{-# INLINE reject #-}
 
 and :: LTL a -> LTL a -> LTL a
-and = And
+and = combine
+{-# INLINE and #-}
 
 or :: LTL a -> LTL a -> LTL a
-or = Or
+or = select
+{-# INLINE or #-}
 
 next :: LTL a -> LTL a
-next = Next
+next = Delay
+{-# INLINE next #-}
 
 until :: LTL a -> LTL a -> LTL a
-until = Until
+until p q = or q (and p (next (until p q)))
+{-# INLINE until #-}
 
 release :: LTL a -> LTL a -> LTL a
-release = Release
+release p q = and q (or p (next (release p q)))
+{-# INLINE release #-}
 
 implies :: LTL a -> LTL a -> LTL a
-implies p q = Or (neg p) q
+implies p = or (neg p)
+{-# INLINE implies #-}
 
 eventually :: LTL a -> LTL a
-eventually p = Until Top p
+eventually = until top
+{-# INLINE eventually #-}
 
 always :: LTL a -> LTL a
-always p = Release (Bottom "always") p
+always = release (bottom "always")
+{-# INLINE always #-}
 
 truth :: Bool -> LTL a
-truth b = if b then Top else Bottom "truth"
+truth b = if b then top else bottom "truth"
+{-# INLINE truth #-}
 
 eq :: Eq a => a -> LTL a
-eq n = Accept $ truth . (== n)
+eq n = accept $ truth . (== n)
+{-# INLINE eq #-}
 
 test :: Machine Int (Result Int)
-test = run (compile (implies (eq 2) (Bottom "here"))) [2]
+test = run (always (implies (eq 2) (bottom "here"))) [1,2,3,4]
