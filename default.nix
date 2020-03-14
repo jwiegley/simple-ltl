f144be826d9696f678782dc6e342e66cfbe10bcd
John Wiegley <johnw@newartisans.com>
2020-03-14 10:09:52 -0700

    Add system argument to default.nix

--- default.nix
+++ default.nix
@@ -1,11 +1,13 @@
 { compiler ? "ghc882"
 
+, system ? builtins.currentSystem
 , rev    ? "8da81465c19fca393a3b17004c743e4d82a98e4f"
 , sha256 ? "1f3s27nrssfk413pszjhbs70wpap43bbjx2pf4zq5x2c1kd72l6y"
 , pkgs   ?
     import (builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
       inherit sha256; }) {
+      inherit system;
       config.allowUnfree = true;
       config.allowBroken = false;
     }
