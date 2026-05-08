b0b1a0bd82f42494d63270cf27940702d858e67b
John Wiegley <johnw@newartisans.com>
2026-05-08 11:13:44 -0700

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
-{ ghcCompiler ? "ghc8104"
+{ ghcCompiler ? "ghc8107"
 
-, rev    ? "c74fa74867a3cce6ab8371dfc03289d9cc72a66e"
-, sha256 ? "13bnmpdmh1h6pb7pfzw5w3hm6nzkg9s1kcrwgw1gmdlhivrmnx75"
+, rev    ? "a3a23d9599b0a82e333ad91db2cdc479313ce154"
+, sha256 ? "05xmgrrnw6j39lh3d48kg064z510i0w5vvrm1s5cdwhdc2fkspjq"
 , pkgs   ? import (builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
     inherit sha256; }) {
@@ -10,7 +10,7 @@
   }
 
 , returnShellEnv ? pkgs.lib.inNixShell
-, mkDerivation   ? null
+, mkDerivation ? null
 }:
 
 let haskellPackages = pkgs.haskell.packages.${ghcCompiler};
@@ -28,8 +28,6 @@ in haskellPackages.developPackage rec {
       haskellPackages.hpack
       haskellPackages.hoogle
       haskellPackages.hasktags
-      haskellPackages.ghcid
-      haskellPackages.ormolu
     ];
 
     benchmarkDepends = (attrs.benchmarkDepends or []) ++ [
